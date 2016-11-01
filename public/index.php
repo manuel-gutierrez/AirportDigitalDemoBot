be5318a5eaf403cff3289695c6f00359f24e2116
Manuel Enrique <menrique.gutierrez@gmail.com>
2016-11-01 10:00:50 -0500

    Map implementation: server error debug

--- public/index.php
+++ public/index.php
@@ -9,9 +9,11 @@ if (PHP_SAPI == 'cli-server') {
     }
 }
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . './../vendor/autoload.php';
+
+
 // Instantiate the app
-$settings = require __DIR__ . '/../src/settings.php';
+$settings = require __DIR__ . './../src/settings.php';
 $app = new \Slim\App($settings);
 
 //Start a session : For future implementations. 
@@ -19,13 +21,13 @@ $app->add(new \RKA\SessionMiddleware(['name' => 'request']));
 
 
 // Set up dependencies
-require __DIR__ . '/../src/dependencies.php';
+require __DIR__ . './../src/dependencies.php';
 
 // Register middleware
-require __DIR__ . '/../src/middleware.php';
+require __DIR__ . './../src/middleware.php';
 
 // Register routes
-require __DIR__ . '/../src/routes.php';
+require __DIR__ . './../src/routes.php';
 
 
 
