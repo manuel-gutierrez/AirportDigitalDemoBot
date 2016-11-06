3dcd7f24eea021e92ef55278a57506268d4e1c91
Manuel Enrique <menrique.gutierrez@gmail.com>
2016-11-06 18:45:21 -0500

    Maps: Card Structure changed

--- src/Controllers/MapController.php
+++ src/Controllers/MapController.php
@@ -103,7 +103,7 @@ class MapController
 		    if (!empty($venue->terminal) && !empty($venue->hours)) {
 			
 			    if (!empty($venue->gate)) {
-			    	$element["subtitle"] = $venue->terminal." | ". $venue->gate.'<br> Open '. $venue->hours;
+			    	$element["subtitle"] = $venue->terminal." | ". $venue->gate.' | Open '. $venue->hours;
 			    } else {
 			    		$element["subtitle"] = $venue->terminal." | Open ". $venue->hours;
 		   		 }
